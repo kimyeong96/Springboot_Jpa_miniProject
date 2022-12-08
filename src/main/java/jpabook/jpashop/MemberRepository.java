c451fad50d850940b17b60c9cc5dfc8b40176bfe
kimyeong96 <marsincat1405@gmail.com>
2022-12-08 16:47:03 +0900

    create entity

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -12,14 +12,14 @@ public class MemberRepository{
     @PersistenceContext
     private EntityManager em;
 
-    public Long save(Member member) {
-        em.persist(member);
-        return member.getId();
-    }
-
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
+//    public Long save(Member member) {
+//        em.persist(member);
+//        return member.getId();
+//    }
+//
+//    public Member find(Long id) {
+//        return em.find(Member.class, id);
+//    }
 
 
 }
